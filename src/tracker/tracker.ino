3dd6686d370da6309ad972baf308b8232cee7f0f
voidcaller <a.soeyler@fu-berlin.de>
2020-02-16 15:14:30 +0100

    removed all functions that are not needed for RFID reading

--- src/tracker/tracker.ino
+++ src/tracker/tracker.ino
@@ -2,17 +2,6 @@
 #include <SPI.h>
 #include <Adafruit_PN532.h>
 
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-
-#include <ESP8266HTTPClient.h>
-
-#include <WiFiClient.h>
-
-#include <WiFiSecrets.h>
-
-ESP8266WiFiMulti WiFiMulti;
-
 // If using the breakout with SPI, define the pins for SPI communication.
 #define PN532_SCK  (2)
 #define PN532_MOSI (4)
@@ -23,10 +12,6 @@ ESP8266WiFiMulti WiFiMulti;
 #define RED (14)
 #define GREEN (13)
 #define BLUE (12)
-
-// Define Backend url
-
-#define BACKEND_URL ("http://jigsaw.w3.org/HTTP/connection.html")
 
 // If using the breakout or shield with I2C, define just the pins connected
 // to the IRQ and reset lines.
@@ -75,21 +60,6 @@ void setup(void) {
   
   Serial.println("Waiting for an ISO14443A Card ...");
 
-  // starting WIFI
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
-  }
-
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP(SECRET_SSID, SECRET_PASSWORD);
-
-  // wait for Wifi connection
-  while ((WiFiMulti.run() != WL_CONNECTED)) {
-    delay(100);
-  }
-
   //ending start up phase
   digitalWrite(BLUE, LOW);
 }
@@ -115,50 +85,7 @@ void loop(void) {
     Serial.print("  UID Value: ");
     nfc.PrintHex(uid, uidLength);
     Serial.println("");
-
-    // post uid to backend
-    if ((WiFiMulti.run() == WL_CONNECTED)) {
-
-    WiFiClient client;
-
-    HTTPClient http;
-
-    Serial.print("[HTTP] begin...\n");
-    if (http.begin(client, BACKEND_URL)) {  // HTTP
-
-
-      Serial.print("[HTTP] GET...\n");
-      // start connection and send HTTP header
-      int httpCode = http.GET();
-
-      // httpCode will be negative on error
-      if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
-        // file found at server
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          String payload = http.getString();
-          Serial.println(payload);
-          digitalWrite(BLUE, LOW);
-          blink(GREEN, 500);
-        }
-      } else {
-        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        digitalWrite(BLUE, LOW);
-        blink(RED, 500);
-      }
-
-      http.end();
-    } else {
-      Serial.printf("[HTTP} Unable to connect\n");
-      digitalWrite(BLUE, LOW);
-      blink(RED, 500);
-    }
-  }
-    
-  }
-  else {
+  } else {
     Serial.println("Unable to read card!");
     blink(RED, 500);
   }
